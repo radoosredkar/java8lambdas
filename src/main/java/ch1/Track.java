4284fc0fc3aebc6d58a4f2d3b24c79d92caf2576
rado <radoosredkar@hotmail.com>
2022-06-05 20:23:21 +0200

    Chapter 1

---
+++ src/main/java/ch1/Track.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ch1;
+
+/**
+ * @author richard
+ */
+public final class Track {
+    
+    private final String name;
+    private final int length;
+
+    public Track(String name, int length) {
+        this.name = name;
+        this.length = length;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the length of the track in milliseconds.
+     */
+    public int getLength() {
+        return length;
+    }
+
+    public Track copy() {
+        return new Track(name, length);
+    }
+
+    @Override
+    public String toString() {
+        return "Track{" +
+                "name='" + name + '\'' +
+                ", length=" + length +
+                '}';
+    }
+}
