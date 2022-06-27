caa322ee517b03a3ce13ed4a5361af311f274189
Rado Osredkar <r.osredkar@sportradar.com>
2022-06-27 18:42:51 +0200

    Chapter 8

---
+++ src/main/java/ch8/command/Editor.java
@@ -0,0 +1,7 @@
+package ch8.command;
+
+public interface Editor {
+    public void save();
+    public void open();
+    public void close();
+}
---
+++ src/main/java/ch8/strategy/ZipCompressionStrategy.java
@@ -0,0 +1,12 @@
+package ch8.strategy;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.zip.ZipOutputStream;
+
+public class ZipCompressionStrategy implements CompressionStrategy {
+    @Override
+    public OutputStream compress(OutputStream data) throws IOException {
+        return new ZipOutputStream(data);
+    }
+}
